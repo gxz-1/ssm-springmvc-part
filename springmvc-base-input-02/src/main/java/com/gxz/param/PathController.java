809eb004480bf62e7841da82b8203ac6364131da
gxz_pc <xizhuogao@foxmail.com>
2023-12-16 17:40:55 +0800

    springMVC P113 over!

--- springmvc-base-input-02/src/main/java/com/gxz/param/PathController.java
+++ springmvc-base-input-02/src/main/java/com/gxz/param/PathController.java
@@ -13,7 +13,8 @@ public class PathController {
 
     @RequestMapping("{account}/{password}")//设置动态路径，并在形参中接收
     //设置@PathVariable注解，表示接收路径参数而不是param
-    public String login(@PathVariable(value = "account") String name,@PathVariable(required = false) String password){
+    public String login(@PathVariable(value = "account") String name,
+                        @PathVariable(required = false) String password){
         System.out.println("name = " + name + ", password = " + password);
         return "name = " + name + ", password = " + password;
     }
